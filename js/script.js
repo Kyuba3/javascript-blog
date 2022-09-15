74640f3e01adc79bf2647dc3e1d0a2ec0fa9bea6
Kuba Kołodziej <jmk.kolodziej@gmail.com>
2022-09-15 12:25:37 +0200

    Add StyleLint into project

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-  
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
@@ -15,13 +15,13 @@
     /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
-  
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
-    
+
     /* get 'href' attribute from the clicked link */
     const linkAtribiute = clickedElement.getAttribute('href');
     console.log(linkAtribiute);
@@ -32,13 +32,13 @@
 
     /* add class 'active' to the correct article */
     targetArticle.classList.add('active');
-    console.log('Adding class "active" to: ' + linkAtribiute);    
+    console.log('Adding class "active" to: ' + linkAtribiute);
   };
-  
+
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
-  
+
   function generateTitleLinks(){
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
@@ -59,7 +59,7 @@
       html = html + linkHTML;
       //console.log(html);
     }
- 
+
     titleList.innerHTML = html;
 
     const links = document.querySelectorAll('.titles a');
